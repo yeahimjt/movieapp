e4ef864be8d793a272143a651308e665cfaa2e27
yeahimjt <80553412+yeahimjt@users.noreply.github.com>
2023-03-12 05:20:19 -0500

    Updating for GH-Pages

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <Router>
+      <Router basename={window.location.pathname || ''}>
         <App />
       </Router>
     </Provider>
