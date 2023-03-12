41f091726577af1e96fd831fbf8a92e70755b50c
yeahimjt <80553412+yeahimjt@users.noreply.github.com>
2023-03-12 07:19:03 -0500

    Fixing GitHub Pages

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename={'https://yeahimjt.github.io/movieapp'}>
+      <BrowserRouter basename={window.location.pathname || ''}>
         <App />
       </BrowserRouter>
     </Provider>
