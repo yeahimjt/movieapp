f3b0efe3f947028275ae87d45bb270e5b702b297
yeahimjt <80553412+yeahimjt@users.noreply.github.com>
2023-03-12 06:57:25 -0500

    .

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename={'https://yeahimjt.github.io/movieapp/'}>
+      <BrowserRouter basename={'https://yeahimjt.github.io/movieapp'}>
         <App />
       </BrowserRouter>
     </Provider>
